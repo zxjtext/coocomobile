18dee37540c4a1c7e4a96b4be8523c30a6abdbad
Jerry Zhang <jerry_zhang@mtelnet.com>
2019-10-11 14:14:34 +0800

    提交内容

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFTest
+//
+//  Created by zxj on 17/4/5.
+//  Copyright © 2017年 zxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AFTest
+//
+//  Created by zxj on 17/4/5.
+//  Copyright © 2017年 zxj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
